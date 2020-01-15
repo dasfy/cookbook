851c68a59612e7f52b69a0ebf5860b86a0a98cde
dasfy43@yandex.ru <dasfy43@yandex.ru>
2020-01-15 21:27:54 +0300

    moe

--- cookbook/cookbook/Form1.Designer.cs
+++ cookbook/cookbook/Form1.Designer.cs
@@ -38,6 +38,15 @@
             this.ingred4C = new System.Windows.Forms.ComboBox();
             this.ingred5C = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.measure5T = new System.Windows.Forms.TextBox();
+            this.measure4T = new System.Windows.Forms.TextBox();
+            this.measure3T = new System.Windows.Forms.TextBox();
+            this.measure2T = new System.Windows.Forms.TextBox();
+            this.measure1T = new System.Windows.Forms.TextBox();
             this.measure5C = new System.Windows.Forms.ComboBox();
             this.measure4C = new System.Windows.Forms.ComboBox();
             this.measure3C = new System.Windows.Forms.ComboBox();
@@ -50,15 +59,9 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.measure1T = new System.Windows.Forms.TextBox();
-            this.measure2T = new System.Windows.Forms.TextBox();
-            this.measure3T = new System.Windows.Forms.TextBox();
-            this.measure4T = new System.Windows.Forms.TextBox();
-            this.measure5T = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.howtoC = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -76,7 +79,7 @@
             // titleT
             // 
             this.titleT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.titleT.Location = new System.Drawing.Point(12, 41);
+            this.titleT.Location = new System.Drawing.Point(12, 12);
             this.titleT.Name = "titleT";
             this.titleT.Size = new System.Drawing.Size(154, 23);
             this.titleT.TabIndex = 1;
@@ -85,7 +88,7 @@
             // 
             this.autorC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.autorC.FormattingEnabled = true;
-            this.autorC.Location = new System.Drawing.Point(12, 78);
+            this.autorC.Location = new System.Drawing.Point(12, 88);
             this.autorC.Name = "autorC";
             this.autorC.Size = new System.Drawing.Size(154, 24);
             this.autorC.TabIndex = 2;
@@ -94,7 +97,7 @@
             // 
             this.kitchenC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.kitchenC.FormattingEnabled = true;
-            this.kitchenC.Location = new System.Drawing.Point(12, 117);
+            this.kitchenC.Location = new System.Drawing.Point(12, 126);
             this.kitchenC.Name = "kitchenC";
             this.kitchenC.Size = new System.Drawing.Size(154, 24);
             this.kitchenC.TabIndex = 2;
@@ -171,6 +174,86 @@
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button3.Location = new System.Drawing.Point(15, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(34, 27);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(273, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Ед. измерения";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Кол-во";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(55, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ингредиенты";
+            // 
+            // measure5T
+            // 
+            this.measure5T.Location = new System.Drawing.Point(198, 197);
+            this.measure5T.Multiline = true;
+            this.measure5T.Name = "measure5T";
+            this.measure5T.Size = new System.Drawing.Size(59, 24);
+            this.measure5T.TabIndex = 8;
+            // 
+            // measure4T
+            // 
+            this.measure4T.Location = new System.Drawing.Point(198, 161);
+            this.measure4T.Multiline = true;
+            this.measure4T.Name = "measure4T";
+            this.measure4T.Size = new System.Drawing.Size(59, 24);
+            this.measure4T.TabIndex = 7;
+            // 
+            // measure3T
+            // 
+            this.measure3T.Location = new System.Drawing.Point(198, 123);
+            this.measure3T.Multiline = true;
+            this.measure3T.Name = "measure3T";
+            this.measure3T.Size = new System.Drawing.Size(59, 24);
+            this.measure3T.TabIndex = 6;
+            // 
+            // measure2T
+            // 
+            this.measure2T.Location = new System.Drawing.Point(198, 85);
+            this.measure2T.Multiline = true;
+            this.measure2T.Name = "measure2T";
+            this.measure2T.Size = new System.Drawing.Size(59, 24);
+            this.measure2T.TabIndex = 5;
+            // 
+            // measure1T
+            // 
+            this.measure1T.Location = new System.Drawing.Point(198, 47);
+            this.measure1T.Multiline = true;
+            this.measure1T.Name = "measure1T";
+            this.measure1T.Size = new System.Drawing.Size(59, 24);
+            this.measure1T.TabIndex = 4;
+            // 
             // measure5C
             // 
             this.measure5C.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
@@ -220,7 +303,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(184, 41);
+            this.label1.Location = new System.Drawing.Point(184, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(115, 17);
             this.label1.TabIndex = 5;
@@ -230,7 +313,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(184, 80);
+            this.label2.Location = new System.Drawing.Point(183, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 17);
             this.label2.TabIndex = 5;
@@ -240,7 +323,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label3.Location = new System.Drawing.Point(184, 117);
+            this.label3.Location = new System.Drawing.Point(184, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 17);
             this.label3.TabIndex = 5;
@@ -270,7 +353,7 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button1.Location = new System.Drawing.Point(245, 75);
+            this.button1.Location = new System.Drawing.Point(245, 85);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(34, 27);
             this.button1.TabIndex = 7;
@@ -283,98 +366,52 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button2.Location = new System.Drawing.Point(245, 112);
+            this.button2.Location = new System.Drawing.Point(245, 124);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(34, 27);
             this.button2.TabIndex = 8;
             this.button2.Text = "+";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // measure1T
+            // howtoC
             // 
-            this.measure1T.Location = new System.Drawing.Point(198, 47);
-            this.measure1T.Multiline = true;
-            this.measure1T.Name = "measure1T";
-            this.measure1T.Size = new System.Drawing.Size(59, 24);
-            this.measure1T.TabIndex = 4;
+            this.howtoC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.howtoC.FormattingEnabled = true;
+            this.howtoC.Location = new System.Drawing.Point(12, 50);
+            this.howtoC.Name = "howtoC";
+            this.howtoC.Size = new System.Drawing.Size(154, 24);
+            this.howtoC.TabIndex = 9;
             // 
-            // measure2T
+            // label8
             // 
-            this.measure2T.Location = new System.Drawing.Point(198, 85);
-            this.measure2T.Multiline = true;
-            this.measure2T.Name = "measure2T";
-            this.measure2T.Size = new System.Drawing.Size(59, 24);
-            this.measure2T.TabIndex = 5;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label8.Location = new System.Drawing.Point(184, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(168, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "- Способ приготовления";
             // 
-            // measure3T
+            // button4
             // 
-            this.measure3T.Location = new System.Drawing.Point(198, 123);
-            this.measure3T.Multiline = true;
-            this.measure3T.Name = "measure3T";
-            this.measure3T.Size = new System.Drawing.Size(59, 24);
-            this.measure3T.TabIndex = 6;
-            // 
-            // measure4T
-            // 
-            this.measure4T.Location = new System.Drawing.Point(198, 161);
-            this.measure4T.Multiline = true;
-            this.measure4T.Name = "measure4T";
-            this.measure4T.Size = new System.Drawing.Size(59, 24);
-            this.measure4T.TabIndex = 7;
-            // 
-            // measure5T
-            // 
-            this.measure5T.Location = new System.Drawing.Point(198, 197);
-            this.measure5T.Multiline = true;
-            this.measure5T.Name = "measure5T";
-            this.measure5T.Size = new System.Drawing.Size(59, 24);
-            this.measure5T.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(55, 20);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Ингредиенты";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(205, 20);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 13);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Кол-во";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(273, 20);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(82, 13);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Ед. измерения";
-            // 
-            // button3
-            // 
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button3.Location = new System.Drawing.Point(15, 14);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(34, 27);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "+";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button4.Location = new System.Drawing.Point(318, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(34, 27);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(728, 338);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.howtoC);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
@@ -429,6 +466,9 @@
         private System.Windows.Forms.TextBox measure3T;
         private System.Windows.Forms.TextBox measure2T;
         private System.Windows.Forms.TextBox measure1T;
+        private System.Windows.Forms.ComboBox howtoC;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
     }
 }
 
